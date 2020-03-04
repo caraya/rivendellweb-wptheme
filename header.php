c7761b3c6e5382e640ed783377d5c75a677448ea
Carlos <carlos.araya@gmail.com>
2020-03-04 15:01:55 -0800

    last known good image header

--- header.php
+++ header.php
@@ -31,22 +31,22 @@
 	<?php endif ?>
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
-			<?php the_custom_logo() ?>
+			<?php the_custom_logo(); ?>
 			<div class="site-branding__text">
 				<?php if ( is_front_page() && is_home() ) : ?>
-					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<?php
-				else :
-					?>
-					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<?php
-				endif;
-				$rivendellweb_description = get_bloginfo( 'description', 'display' );
-				if ( $rivendellweb_description || is_customize_preview() ) :
-					?>
-					<p class="site-description"><?php echo $rivendellweb_description; /* WPCS: xss ok. */ ?></p>
-				<?php endif; ?>
-			</div> <!-- .site-branding__text -->
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<?php
+			else :
+				?>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<?php
+			endif;
+			$rivendellweb_description = get_bloginfo( 'description', 'display' );
+			if ( $rivendellweb_description || is_customize_preview() ) :
+				?>
+				<p class="site-description"><?php echo $rivendellweb_description; /* WPCS: xss ok. */ ?></p>
+			<?php endif; ?>
+			</div> <!-- .site-branding__text" -->
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
