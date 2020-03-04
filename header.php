c780dc38bbf927e272dce2d125436cacdf4f7308
Carlos <carlos.araya@gmail.com>
2020-03-03 16:43:51 -0800

    Logo Step 1

--- header.php
+++ header.php
@@ -30,8 +30,8 @@
 		</figure>
 	<?php endif ?>
 	<header id="masthead" class="site-header">
-		<?php the_custom_logo();?>
 		<div class="site-branding">
+			<?php the_custom_logo() ?>
 			<div class="site-branding__text">
 				<?php if ( is_front_page() && is_home() ) : ?>
 					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
