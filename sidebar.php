712ca0671ffd003be48cf298df53bada33815f57
Carlos <carlos.araya@gmail.com>
2020-03-15 07:44:23 -0700

    finished basic sidebar layout css

--- sidebar.php
+++ sidebar.php
@@ -6,19 +6,20 @@
  *
  * @package rivendellweb
  */
-
+// If neither sidebar-1 or sidebar-2 are active
+// then bail, nothing for us to do here
 if ( ( ! is_active_sidebar( 'sidebar-1' ) ) ||
 		 ( ! is_active_sidebar( 'sidebar-2' ) ) ) {
 	return;
 }
+
+// Otherwise add the sidebars
 ?>
 
-<?php if ( is_single() || is_front_page() || is_home() ) : ?>
-	<aside id="sidebar1" class="widget-area sidebar1">
-		<?php dynamic_sidebar( 'sidebar-1' ); ?>
-	</aside><!-- sidebar1 -->
+<aside id="sidebar1" class="widget-area sidebar1">
+	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+</aside><!-- sidebar1 -->
 
-	<aside id="sidebar2" class="widget-area sidebar2">
-		<?php dynamic_sidebar( 'sidebar-2' ); ?>
-	</aside><!-- sidebar2 -->
-<?php endif; ?>
+<aside id="sidebar2" class="widget-area sidebar2">
+	<?php dynamic_sidebar( 'sidebar-2' ); ?>
+</aside><!-- sidebar2 -->
