edcc978791dd557d9daabb664b762ec57c37f34e
Carlos <carlos.araya@gmail.com>
2023-06-24 03:22:06 -0700

    Add function to remove block library scripts and css from front end

--- functions.php
+++ functions.php
@@ -231,6 +231,16 @@ endif;
 add_action( 'after_setup_theme', 'rivendellweb_setup' );
 
 /**
+ * Remove Gutenberg Block Library CSS from loading on the frontend
+ */
+function smartwp_remove_wp_block_library_css(){
+ wp_dequeue_style( 'wp-block-library' );
+ wp_dequeue_style( 'wp-block-library-theme' );
+ wp_dequeue_style( 'wc-blocks-style' ); // Remove WooCommerce block CSS
+} 
+add_action( 'wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css', 100 );
+
+/**
  * Set the content width in pixels, based on the theme's design and stylesheet.
  *
  * Priority 0 to make it available to lower priority callbacks.
