68a3230422da707a5bc4d3e112af917949addd8c
Carlos <carlos.araya@gmail.com>
2023-06-24 14:56:02 -0700

    additional tweaks to style removal function

--- functions.php
+++ functions.php
@@ -230,18 +230,6 @@ if ( ! function_exists( 'rivendellweb_setup' ) ) :
 endif;
 add_action( 'after_setup_theme', 'rivendellweb_setup' );
 
-/**
- * Stops Gutenberg Block Library CSS from loading on the frontend
- * 
- * Source: 
- */
-function rivendellweb_remove_wp_block_library_css(){
- wp_dequeue_style( 'wp-block-library-css');
- wp_dequeue_style( 'wp-block-library' );
- wp_dequeue_style( 'wp-block-library-theme' );
- wp_dequeue_style( 'wc-blocks-style' ); // Remove WooCommerce block CSS
-} 
-add_action( 'wp_enqueue_scripts', 'rivendellweb_remove_wp_block_library_css', 100 );
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
@@ -402,7 +390,20 @@ function rivendellweb_js_defer_attr( $tag ) {
 }
 add_filter( 'script_loader_tag', 'rivendellweb_js_defer_attr', 10 );
 
+/**
+ * Stops Gutenberg Block Library CSS from loading on the frontend
+ * 
+ * Source: https://smartwp.com/remove-gutenberg-css/
+ */
+function rivendellweb_remove_styles_css(){
+  wp_dequeue_style( 'wp-block-library-css');
+  wp_dequeue_style( 'wp-block-library' );
+  wp_dequeue_style( 'wp-block-library-theme' );
+  wp_dequeue_style( 'wc-blocks-style' ); // Remove WooCommerce block CSS
+ } 
 
+add_action( 'wp_enqueue_scripts', 'rivendellweb_remove_styles_css', 100 );
+ 
 /**
  * Sets the length of the excerpt in archives and indexes.
  *
